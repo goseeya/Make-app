83604fa217ddb69eb2df34f7639b2c66e0578274
mrakowska001 <malgorzata.rakowska@pwc.com>
2019-09-26 15:41:21 +0200

    Add input

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyMakeupUp
+# Make-app
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.6.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-makeup-up app is running!');
+    expect(page.getTitleText()).toEqual('make-app app is running!');
   });
 
   afterEach(async () => {
